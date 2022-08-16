07c5c26bb40caa5d3c5528a7bfeba9963aa67f98
Samuel Jiménez <jimsamuel550@gmail.com>
2022-08-16 18:02:11 -0500

    Agregué condicionales a la función 'seleccionarMascotaJugador' para seleccionar mascota

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,18 @@
-function seleccionarMascotaJugador() {
-  alert("Seleccionaste tu mascota");
+function iniciarJuego() {
+  let botonMascotaJugador = document.getElementById("botónMascota");
+  botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 }
-let botonMascotaJugador = document.getElementById("botónMascota");
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador() {
+  if (document.getElementById("ratigueya").checked == true) {
+    alert("Seleccionaste a ratigueya.");
+  } else if (document.getElementById("hipodoge").checked == true) {
+    alert("Seleccionaste a hipodogue.");
+  } else if (document.getElementById("capipepo").checked == true) {
+    alert("Seleccionaste a capipepo.");
+  } else {
+    alert("Por favor, selecciona una mascota.");
+  }
+}
+
+window.addEventListener("load", iniciarJuego);
